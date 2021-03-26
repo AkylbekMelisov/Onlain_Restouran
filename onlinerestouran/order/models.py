99302e1288c34c51c34f421fc3407b28afdcdffb
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-03-26 17:20:52 +0600

    create model for order

--- onlinerestouran/order/models.py
+++ onlinerestouran/order/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    statuses = (
+        ('in_prossec', 'in_prossec'),
+        ('ready', 'ready'),
+        ('finish', 'finish')
+    )
+    payment_types = (
+        ('cash', 'cash'),
+        ('card', 'card')
+    )
+    total_sum = models.PositiveIntegerField()
+    date_create = models.DateTimeField()
+    status = models.CharField(choices=statuses, max_length=50, default='in_prossec')
+    payment = models.CharField(choices=payment_types, max_length=50, default='cash')
+    promocode = models.CharField(max_length=50)
+
