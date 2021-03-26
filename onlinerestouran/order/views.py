16083935504ec479ab7fb6f610487ce36e39a4ed
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-03-26 17:18:21 +0600

    create view for Order

--- onlinerestouran/order/views.py
+++ onlinerestouran/order/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+
+class OrderView(APIView):
+    def get(self, request, *args, **kwargs):
+        order = Order.objects.all()
+        serializer = OrderSerializer(order, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"data": "Order create successfully"})
+        return Response(serializer.errors)
