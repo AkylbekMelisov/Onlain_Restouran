dd2fb919cbf9d56d6abe2a8fbcfd0217d2e1744f
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-03-26 16:41:08 +0600

    create serializer for MealSerializer and CategoryDetailSerializer

--- onlinerestouran/online_restouran/serializers.py
+++ onlinerestouran/online_restouran/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from .models import *
 
-class CategorySerializers(serializers.ModelSerializer):
+
+class CategorySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Category
         fields = ['name']
+
+class MealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meal
+        fields = '__all__'
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    meal_set = MealSerializer(many=True)
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'meal_set']
+
